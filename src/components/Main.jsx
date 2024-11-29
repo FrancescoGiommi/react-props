b978f5724b249dcec0c954586e3179e1112cd802
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-29 17:17:17 +0100

    Add filter and map

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,19 @@ import { posts } from "../Posts/posts";
 /* Main export */
 export default function Main() {
   const postFilter = posts.filter((post) => post.published === true);
-
+  const postMap = postFilter.map((post) => post);
   return (
     <>
       <main>
         <div className="container d-flex gap-3 flex-wrap justify-content-center ">
           {posts.map((post) => (
             <Card
-              key={post.id}
-              title={post.title}
-              image={post.image}
-              description={post.content}
-              tags={post.tags}
-              published={post.postFilter}
+              key={postMap.id}
+              title={postMap.title}
+              image={postMap.image}
+              description={postMap.content}
+              tags={postMap.tags}
+              published={postMap.published}
             />
           ))}
         </div>
